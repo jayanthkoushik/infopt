4a486ac636b4424e1ec23bb3505d014a7894543b
Jayanth Koushik <jnkoushik@gmail.com>
2019-10-09 15:20:53 -0400

    Remove .github from gh-pages

--- .github/utils/papersetup.sh
+++
@@ -1,7 +0,0 @@
-cp paper/main.md /www/
-cp paper/references.bib /www/_includes/
-cp paper/commands.md /www/_includes/
-mkdir -p /www/fig
-convert -antialias -density 300 -quality 100 paper/fig/alg.pdf /www/fig/alg.png
-convert -antialias -density 300 -quality 100 paper/fig/synth.pdf /www/fig/synth.png
-convert -antialias -density 300 -quality 100 paper/fig/mnist.pdf /www/fig/mnist.png
